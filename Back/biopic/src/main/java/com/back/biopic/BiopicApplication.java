c24bc530134322f02ea3e544518d66fe45b7d6c8
mbodji <ibumbodji@gmail.com>
2022-12-14 12:12:15 +0100

    initalisation de la partie backend

---
+++ Back/biopic/src/main/java/com/back/biopic/BiopicApplication.java
@@ -0,0 +1,13 @@
+package com.back.biopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiopicApplication.class, args);
+    }
+
+}
---
+++ Back/biopic/src/test/java/com/back/biopic/BiopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.biopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiopicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
